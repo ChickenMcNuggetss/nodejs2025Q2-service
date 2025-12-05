842c27fbaf0ff01ecd2a784381f634524685bf5d
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-12-06 00:00:41 +0300

    feat: add postgres and nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,14 +6,27 @@ import { TracksModule } from './app/tracks/tracks.module';
 import { ArtistsModule } from './app/artists/artists.module';
 import { AlbumsModule } from './app/albums/albums.module';
 import { FavoritesModule } from './app/favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
+    ConfigModule.forRoot(),
     TracksModule,
     ArtistsModule,
     AlbumsModule,
     FavoritesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV === 'development',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
