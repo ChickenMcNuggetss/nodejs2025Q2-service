98a1d7e67eff5d772e61fc70f9b4d3afb4b659ab
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-12-13 19:37:58 +0300

    feat: add logging to middleware

--- src/app/logging/logger/logger.middleware.ts
+++ src/app/logging/logger/logger.middleware.ts
@@ -1,8 +1,17 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Response } from 'express';
+import { LoggingService } from '../logging.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next: () => void) {
+  constructor(private logger: LoggingService) {}
+
+  use(req: Request, res: Response, next: NextFunction) {
+    res.on('finish', () => {
+      this.logger.log(
+        `[${new Date().toISOString()}] ${req.method} ${req.url} ${JSON.stringify(req.body)} ${req.destination}`,
+      );
+    });
     next();
   }
 }
