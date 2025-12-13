d8333cffba23c80db901d69b94ac2db449eaf9ad
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-12-13 19:52:05 +0300

    feat: add filters, logger and listeners for unhandled exception and rejection into main ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConsoleLogger, Logger, ValidationPipe } from '@nestjs/common';
 import { LoggerErrorInterceptor } from 'pino-nestjs';
+import { CatchEverythingFilter } from './app/filters/exception.filter';
+import { LoggingService } from './app/logging/logging.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger(),
+  });
+  const httpAdapter = app.get(HttpAdapterHost);
+  const logger = app.get(LoggingService);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapter, logger));
   app.useGlobalPipes(new ValidationPipe());
-  app.useLogger(app.get(Logger));
+
+  process.on('uncaughtException', (err) => {
+    logger.error(`Uncaught Exception: ${err.message}`);
+  });
+
+  process.on('unhandledRejection', (err) => {
+    logger.error(`Unhandled Rejection: ${JSON.stringify(err)}`);
+  });
   app.useGlobalInterceptors(new LoggerErrorInterceptor());
   await app.listen(4000);
 }
