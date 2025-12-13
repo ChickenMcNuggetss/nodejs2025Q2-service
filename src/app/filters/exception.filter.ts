6114eaa8c0d7fbf7af473b87557cdb276d96024e
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-12-13 19:49:49 +0300

    feat: add exception filter

---
+++ src/app/filters/exception.filter.ts
@@ -0,0 +1,38 @@
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
+import { LoggingService } from '../logging/logging.service';
+
+@Catch()
+export class CatchEverythingFilter implements ExceptionFilter {
+  constructor(
+    private readonly httpAdapterHost: HttpAdapterHost,
+    private logger: LoggingService,
+  ) {}
+
+  catch(exception: unknown, host: ArgumentsHost): void {
+    const { httpAdapter } = this.httpAdapterHost;
+
+    const context = host.switchToHttp();
+
+    const httpStatus =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const responseBody = {
+      statusCode: httpStatus,
+      timestamp: new Date().toISOString(),
+      path: httpAdapter.getRequestUrl(context.getRequest()),
+    };
+
+    this.logger.error(JSON.stringify(responseBody));
+
+    httpAdapter.reply(context.getResponse(), responseBody, httpStatus);
+  }
+}
