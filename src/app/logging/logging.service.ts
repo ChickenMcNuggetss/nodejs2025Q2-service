d44135bc5a5a4cce9f3c92089f0f50d0b7773271
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-12-13 15:04:58 +0300

    feat: add logger

--- src/app/logging/logging.service.ts
+++ src/app/logging/logging.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class LoggingService {}
+export class LoggingService {
+  private readonly logger = new Logger('loggingService', { timestamp: true });
+
+  log(message: string) {
+    this.logger.log(message);
+  }
+
+  error(message: string) {
+    this.logger.error(`Error: ${message}`);
+  }
+}
